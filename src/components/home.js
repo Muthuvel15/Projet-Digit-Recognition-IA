9b583e9b2bc83b05a08b35049047c1a63936d7d4
Muthuvel <muthuvel.savoundirapandiane@outlook.com>
2023-10-11 10:51:51 +0200

    merge the code home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,9 @@
 import React, { useRef } from 'react';
 import { ReactSketchCanvas } from 'react-sketch-canvas';
+//import {saveAs} from 'file-saver';
 import '../App.css';
+import './header';
+import Header from './header';
 
 const Home = () => {
   const styles = {
@@ -23,10 +26,15 @@ const Home = () => {
     canvasRef.current.clearCanvas();
   };
 
+  const handleSubmit = () => {
+    const canvas = ReactSketchCanvas
+    console.log(canvas)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4">Digit Recognition App</h1>
-      
+      < Header/>
+    <br/> <br/>      
       <div style={canvasWrapperStyles}>
         <ReactSketchCanvas
           ref={canvasRef}
@@ -39,7 +47,7 @@ const Home = () => {
       </div>
       <div>
       <button 
-        onClick={() => {/* Add your prediction logic here */}}
+        onClick={handleSubmit}
         className="mt-4 p-2 bg-blue-500 text-white rounded"
       >
         Predict Digit
@@ -50,9 +58,16 @@ const Home = () => {
         className="mt-4 ml-2 p-2 bg-red-500 text-white rounded"
       >
         clear
-      </button> 
-
+      </button>
       </div>
+      <br/>
+      <div>
+      <h3 className="text-4xl font-bold mb-4">prediction</h3>
+      <p>data</p>
+      </div>
+      
+      
+
     
     </div>
   );
