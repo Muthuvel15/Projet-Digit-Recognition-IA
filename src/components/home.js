0d08d731fec4139f987afc474b4c06e87347154c
nabila-elabdali <nabila.el-abdali@qualiconsult.fr>
2023-10-11 10:44:02 +0200

    update home.js

--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import { ReactSketchCanvas } from 'react-sketch-canvas';
-import '../App.css';
-
+import '../App.css'
+import {saveAs} from 'file-saver'
 const Home = () => {
   const styles = {
     border: '0.0625rem solid #9c9c9c',
@@ -18,11 +18,25 @@ const Home = () => {
   };
 
   const canvasRef = useRef(null);
+ 
+  const handleSubmit = async () => {
+    const canvasData = await canvasRef.current.exportImage('png');
+    console.log(canvasData);
+    saveAs(canvasData, 'digit.png');
+    sendData(canvasData)
+}
 
   const resetCanvas = () => {
     canvasRef.current.clearCanvas();
+    
   };
-
+  //here we were use axios to send data to database
+const sendData=(c)=>{
+  console.log(c)
+}
+const getImageResult=(id)=>{
+  
+}
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold mb-4">Digit Recognition App</h1>
@@ -35,11 +49,13 @@ const Home = () => {
           height='280px'
           strokeWidth={10}
           strokeColor="green"
+          imageFormat='jpg'
+          lineWidth={60}
         />
       </div>
       <div>
       <button 
-        onClick={() => {/* Add your prediction logic here */}}
+        onClick={handleSubmit}
         className="mt-4 p-2 bg-blue-500 text-white rounded"
       >
         Predict Digit
@@ -50,7 +66,7 @@ const Home = () => {
         className="mt-4 ml-2 p-2 bg-red-500 text-white rounded"
       >
         clear
-      </button> 
+      </button>
 
       </div>
     
